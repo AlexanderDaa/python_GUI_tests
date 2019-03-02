f906269c85255d1b4c7ac399a09e338a10b0c66a
AlexanderDaa <12345qazWS>
2019-03-02 09:49:42 +0600

    video 8_3 + случайное имя группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,17 @@
+import random
+import string
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 def test_add_group(app):
-
-    pass
-
+    old_list = app.groups.get_group_list()
+    new_group = random_string("gr_", 10)
+    app.groups.add_new_group(new_group)
+    new_list = app.groups.get_group_list()
+    old_list.append(new_group)
+    assert sorted(old_list) == sorted(new_list)
 
 
